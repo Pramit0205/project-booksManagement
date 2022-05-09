cc3ab7e6d1ec73dface90e13fe3d57e93acf4ab1
Animesh Dey <animesh.dey986@gmail.com>
2022-05-10 00:35:18 +0530

    user

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -7,27 +7,27 @@ const jwt = require("jsonwebtoken")
 let userData = async (req, res) => {
   try {
     let { title, name, phone, email, password, address } = req.body;
-//===================================== if Empty Body ================================================================
+    //===================================== if Empty Body ================================================================
     if (Object.keys(req.body).length == 0)
       return res
         .status(400)
         .send({ status: false, msg: "plz enter User data" });
-//========================================== if Title missing & not Valid=======================================================        
+    //========================================== if Title missing & not Valid=======================================================        
     if (!title)
       return res.status(400).send({ status: false, msg: "title missing" });
-      if (!validation.isValidTitle(title))
+    if (!validation.isValidTitle(title))
       return res
         .status(400)
         .send({ status: false, message: "Not a valid Title" });
-//====================================================== If Name Missing and Not Valid =========================================
+    //====================================================== If Name Missing and Not Valid =========================================
     if (!validation.isValid(name))
       return res.status(400).send({ status: false, msg: "Name is missing" });
-    
+
     if (!validation.isValidName(name))
       return res
         .status(400)
         .send({ status: false, message: "Not a valid Name" });
-//=================================================== If PhONE Missing and Not Valid and ALready Exists =======================================
+    //=================================================== If PhONE Missing and Not Valid and ALready Exists =======================================
     if (!phone)
       return res
         .status(400)
@@ -41,10 +41,10 @@ let userData = async (req, res) => {
       }
     }
     if (!validation.isValidPhone(phone))
-    return res
-      .status(400)
-      .send({ status: false, message: "Not a valid Phone Number" });
-//========================================================== If Email Missing and Not Valid and ALready exists ==================================
+      return res
+        .status(400)
+        .send({ status: false, message: "Not a valid Phone Number" });
+    //========================================================== If Email Missing and Not Valid and ALready exists ==================================
     if (!email)
       return res.status(400).send({ status: false, msg: "email missing" });
     if (email) {
@@ -59,7 +59,7 @@ let userData = async (req, res) => {
       return res
         .status(400)
         .send({ status: false, message: "Not a valid emailId" });
-//=============================================== IF Password Missing and Not Valid  ================================================       
+    //=============================================== IF Password Missing and Not Valid  ================================================       
     if (!password) {
       return res
         .status(400)
@@ -69,30 +69,23 @@ let userData = async (req, res) => {
     if (!validation.isValidPassword(password))
       return res
         .status(400)
-        .send({ status: false, message: "Not a valid password" });
-// =========================================================== Address Validation =============================================
- if(address){
-    if (!validation.isValid(address)) {
+        .send({ status: false, message: "Your password must contain atleast one number,uppercase,lowercase and special character[ @ $ ! % * ? & ] and length should be min of 8-15 charachaters" });
+    // =========================================================== Address Validation =============================================
+    if (address) {
+      if (!validation.isValid(address)) {
         return res
           .status(400)
           .send({ status: false, message: "Address Must Be Present" });
       }
-      if(Object.keys(address).length==0) return res.status(400).send({status:false, msg: "Address Field cannot Be empty"}); 
-    if(typeof address !== "Object") return res.status(400).send({status:false , msg : "Address must be an Object"})
- }
+      if (Object.keys(address).length == 0) return res.status(400).send({ status: false, msg: "Address Field cannot Be empty" });
+      if (typeof address !== "Object") return res.status(400).send({ status: false, msg: "Address must be an Object" })
+    }
 
-    
-//================================================================ Data creation ============================================
-    const result = await userModel.create({
-      title,
-      name,
-      phone,
-      email,
-      password,
-      address,
-    });
+    //================================================================ Data creation ============================================
+    const result = await userModel.create({ title, name, phone, email, password, address });
     res.status(201).send({ status: true, data: result });
-  } catch (err) {
+  } 
+  catch (err) {
     return res.status(500).send({ statuS: false, msg: err.message });
   }
 };
@@ -100,42 +93,43 @@ let userData = async (req, res) => {
 
 //Login User
 const loginUser = async function (req, res) {
-    try {
-        const credentials = req.body
-        
-        // Validation of input
-        if (!validation.isValidRequest(credentials)) return res.status(400).send({ status: false, message: "Please enter the required credentials." })
-        const { email, password } = credentials
-       
-        // Input Value Validation
-        if (!validation.isValid(email)) return res.status(400).send({ status: false, message: "Please enter the emailId" })
-        if (!validation.isValid(password))return res.status(400).send({ status: false, message: "Please enter the password" })
-        
-        // Validation email & password 
-        if (!validation.isValidEmail(email)) return res.status(400).send({ status: false, message: "Not a valid emailId" })
-        if (!validation.isValidPassword(password))return res.status(400).send({ status: false, message: "Not a valid password" })
+  try {
+    const credentials = req.body
 
-        // Finding the user
-        const getUser = await userModel.findOne({ email })
-        if (!getUser) return res.status(404).send({ status: false, message: "User email id not found." })
-        
-        // Checking the password
-        const matchPassword = await userModel.findOne({password})
-        if(!matchPassword) return res.status(401).send({ status: false, message: "Incorrect password"})
-       
-        // Token Generation
-        const token = await jwt.sign({
-            userId: getUser._id,
-            iat: Math.floor(Date.now()/1000),
-            exp: Math.floor(Date.now()/1000)+ 20*60*60
-        }, "UrAnIuM#GrOuP@32");
-       
-        // Set header
-        res.setHeader("x-api-key", token);
-        res.status(200).send({ status: true, message: "Author login successful", data: { token } })
-    } catch (err) {
-        res.status(500).send({ status: false, message: "Error", error: err.message })
-    }
+    // Validation of input
+    if (!validation.isValidRequest(credentials)) return res.status(400).send({ status: false, message: "Please enter the required credentials." })
+    const { email, password } = credentials
+
+    // Input Value Validation
+    if (!validation.isValid(email)) return res.status(400).send({ status: false, message: "Please enter the emailId" })
+    if (!validation.isValid(password)) return res.status(400).send({ status: false, message: "Please enter the password" })
+
+    // Validation email & password 
+    if (!validation.isValidEmail(email)) return res.status(400).send({ status: false, message: "Not a valid emailId" })
+    if (!validation.isValidPassword(password)) return res.status(400).send({ status: false, message: "Not a valid password" })
+
+    // Finding the user
+    const getUser = await userModel.findOne({ email })
+    if (!getUser) return res.status(404).send({ status: false, message: "User email id not found." })
+
+    // Checking the password
+    const matchPassword = await userModel.findOne({ password })
+    if (!matchPassword) return res.status(401).send({ status: false, message: "Incorrect password" })
+
+    // Token Generation
+    const token = await jwt.sign({
+      userId: getUser._id,
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 20 * 60 * 60
+    }, "UrAnIuM#GrOuP@32");
+
+    // Set header
+    res.setHeader("x-api-key", token);
+    res.status(200).send({ status: true, message: "Author login successful", data: { token } })
+  }
+   catch (err) {
+    res.status(500).send({ status: false, message: "Error", error: err.message })
+  }
 }
 
 module.exports.userData = userData;
