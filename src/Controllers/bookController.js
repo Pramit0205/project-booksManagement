2ad91da3d9cb83300568266de8a02e9be8e02d8f
Animesh Dey <animesh.dey986@gmail.com>
2022-05-10 00:43:04 +0530

    ok

--- src/Controllers/bookController.js
+++ src/Controllers/bookController.js
@@ -76,5 +76,7 @@ const createBook = async function (req, res) {
         res.status(500).send({ status: false, msg: e.message })
     }
 
-    module.exports.createBook = createBook
+   
 }
+
+module.exports.createBook = createBook
