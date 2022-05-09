b21563081fad9e717a8e8c27ba54b12ad77686f3
Animesh Dey <animesh.dey986@gmail.com>
2022-05-09 23:23:09 +0530

    ok

--- src/Routers/routes.js
+++ src/Routers/routes.js
@@ -1,4 +1,3 @@
-const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -13,6 +12,9 @@ router.get("/test-me", function (req, res) {
     res.status(200).send("My server is running awesome!")
 })
 //--------------------------------------------------------//
+
+//User API
 router.post("/registerUser", userController.userData);
+router.post("/login", userController.loginUser)
 
 module.exports = router
