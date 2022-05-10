437f3c532391e832ab0e5e097b66aa33bde5bda0
laxmi dobhal <luckydobhal1@gmail.com>
2022-05-10 11:38:39 +0530

    ReviewSchema

--- src/Models/reviewModel.js
+++ src/Models/reviewModel.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const ReviewSchema = new mongoose.Schema({
 
     bookId: { type: ObjectId, required: true, ref: 'Book' },
-    reviewedBy: { type: String, default: 'Guest', trim: true },
+    reviewedBy: { type: String, default: 'Guest', value: "reviewer's name",trim: true },
     reviewedAt: { type: Date, require: true, default: Date.now() },
     rating: { type: Number, required: true, min: 1, max: 5, trim: true },
     review: { type: String, trim: true },
